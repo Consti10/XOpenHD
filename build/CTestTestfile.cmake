67c3f52183c572c7b2e863ac0c49269cc91e5596
consti10 <geierconstantinabc@gmail.com>
2020-11-28 18:41:06 +0100

    Use CLIon

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/consti10/Desktop/XOpenHD
-# Build directory: /home/consti10/Desktop/XOpenHD/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
