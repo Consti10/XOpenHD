7a7cfa2a9476a7df1a263ae6401f078dfaf69453
consti10 <geierconstantinabc@gmail.com>
2020-11-28 18:44:09 +0100

    use CLIon 3

---
+++ cmake-build-debug/CMakeFiles/xopenhd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xopenhd.dir/main.cpp.o"
+  "xopenhd"
+  "xopenhd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xopenhd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
