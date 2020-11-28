ae443526110f3e2668fb0bc5690f7aaa6c355c81
consti10 <geierconstantinabc@gmail.com>
2020-11-28 19:30:22 +0100

    copy as much as possible from tx_rawsock int WFBHelper namespace 2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,8 @@
 #include <iostream>
-#include "fec.h"
-#include "openhdlib.h"
-//
-#include "fec.h"
-#include "openhdlib.h"
-#include "wifibroadcast.h"
-#include <fcntl.h>
-#include <getopt.h>
-#include <net/if.h>
-#include <netinet/ether.h>
-#include <netpacket/packet.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <time.h>
-#include <unistd.h>
 #include "WFBTransmitter.h"
 
 int main() {
-    WFBTransmitter wfbTransmitter;
+    WFBTransmitter wfbTransmitter({0});
     wfbTransmitter.sendPacket(nullptr,0,0);
     std::cout << "Hello, World!" << std::endl;
     return 0;
