9930f731f9e8024ff48aed82d7c66153cc52e659
consti10 <geierconstantinabc@gmail.com>
2020-11-28 18:16:26 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/consti10/Desktop/XOpenHD
+# Build directory: /home/consti10/Desktop/XOpenHD/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
